9314d62de4a129b1f2ee7d368721aa31586650ce
LouisseKen10 <louisseken10@gmail.com>
2026-01-15 21:59:28 +0800

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+</section>
+</body>
+  <footer>
+      <?php echo "The BDO, BDO Unibank and other BDO-related trademarks are owned by BDO Unibank, Inc. © 2012. All Rights Reserved.";?>
+  </footer>
+
+</html>
